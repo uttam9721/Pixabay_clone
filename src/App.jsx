db57189ed91cc3e7cd9fc29031ae3f23e932458b
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-16 12:26:32 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import Images from './components/images';
+// import React from 'react'
+// import Images from './components/images';
+// import Navbar from './components/Navbar';
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <div> <marquee>i will </marquee> </div>
+
+//       <Images />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React from 'react';
+import Images from './components/Images';
 import Navbar from './components/Navbar';
+// import './App.css'; // Ensure you create or include this CSS file
+
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <div className="container my-4">
+        <div className="scroll-container bg-light text-dark rounded-pill px-3 py-2 shadow-sm">
+          <p className="scroll-text m-0 fw-bold">
+            🚀 I will launch our official website as soon as possible!
+          </p>
+        </div>
+      </div>
+
       <Images />
     </div>
-  )
-}
+  );
+};
 
 export default App;
